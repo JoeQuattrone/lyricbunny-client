c412e903dd9d18db652e05fde99a4423b9dc17bd
Joe Quattrone <joequattrone5@gmail.com>
2019-04-30 14:35:51 -0400

    Imported dependencies set up song routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './containers/Home'
+import Songs from './containers/Songs'
+import Song from './containers/Song'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/songs" component={Songs}/>
+        <Route exact path="/songs/:id" component={Song} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/containers/Song.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Song extends React.Component {
+  render() {
+    return (
+      <h1>Song</h1>
+    )
+  }
+}
+
+export default Song
